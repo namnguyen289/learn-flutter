b93851d5aabbb755b3aa5c5d88ad10dcf35a3057
Nam Nguyen <vunam289@gmail.com>
2022-03-19 00:43:00 +0700

    inital

---
+++ lib/view/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Text("SOME TEXT"),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            SizedBox(
+              width: 200,
+              child: TextField(
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 10.0)),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Check"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
